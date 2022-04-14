0f49c82368c4fda4634b1381e98495dd59557f06
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-14 21:01:01 +0600

    slider carusel add

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -10,7 +10,7 @@ const Services = () => {
   }, []);
   return (
     <div>
-      <h2 className="services-title">Our Services </h2>
+      <h2 className="services-title mt-5">Our Services </h2>
       <div className="services-container">
         {services.map(service => (
           <Service key={service.id} service={service}></Service>
