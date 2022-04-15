35965095848209ab796e46efdb3453f68928b052
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-16 01:06:56 +0600

    protect route and authenticatin added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import logo from "../../../images/logo/logo.png";
 const Header = () => {
+  const [user] = useAuthState(auth)
+  
+  const handleSignOut = () => {
+        signOut(auth)
+  }
   return (
     <>
       <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
@@ -31,9 +39,13 @@ const Header = () => {
             </Nav>
             <Nav>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
-              <Nav.Link as={Link} to="/login">
+              {
+                user ?
+                  <button className="btn btn-link text-decoration-none text-white" onClick={handleSignOut}>Sign out</button>
+                  :
+                <Nav.Link as={Link} to="/login">
                 Login
-              </Nav.Link>
+              </Nav.Link>}
             </Nav>
           </Navbar.Collapse>
         </Container>
